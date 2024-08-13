a729bd65dde8f3471d0b68e961dc68171775fe1f
Mustafa Ahmed <contactmustafa8@gmail.com>
2024-08-13 11:37:50 +0500

    CarePulse

--- types/index.d.ts
+++ types/index.d.ts
@@ -19,22 +19,25 @@ declare interface User extends CreateUserParams {
 
 declare interface RegisterUserParams extends CreateUserParams {
   userId: string;
+  name: string;
+  email: string;
+  phone: string;
   birthDate: Date;
-  gender: Gender;
+  gender: "Male" | "Female" | "Other";
   address: string;
   occupation: string;
   emergencyContactName: string;
   emergencyContactNumber: string;
-  primaryPhysician: string;
-  insuranceProvider: string;
-  insurancePolicyNumber: string;
-  allergies: string | undefined;
-  currentMedication: string | undefined;
-  familyMedicalHistory: string | undefined;
-  pastMedicalHistory: string | undefined;
-  identificationType: string | undefined;
-  identificationNumber: string | undefined;
-  identificationDocument: FormData | undefined;
+  primaryPhysician?: string;
+  insuranceProvider?: string;
+  insurancePolicyNumber?: string;
+  allergies?: string;
+  currentMedication?: string;
+  familyMedicalHistory?: string;
+  pastMedicalHistory?: string;
+  identificationType: string;
+  identificationNumber: string;
+  identificationDocument?: FormData;
   privacyConsent: boolean;
 }
 
